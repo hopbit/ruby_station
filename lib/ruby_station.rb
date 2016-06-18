a7cdf830cea3da3e695f5acaeaea49205a225bcc
Lukasz Siwinski <lsiwinski@gmail.com>
2016-06-18 14:12:23 +0200

    Add my answers for 'Enumerables' exercises

--- lib/ruby_station.rb
+++ lib/ruby_station.rb
@@ -44,11 +44,56 @@ class Article
 end
 
 class ResponseParser
-
   def summarize(api_response)
     api_hits = api_response[:result][:hits]
     api_hits_size = api_hits.size
     api_hits_size < 1 ? "There are no hits" : "Number of hits: #{api_hits_size}; best: #{api_hits.first[:value]}"
   end
+end
 
+class UserMapper
+  def map_over_users(users)
+    users.map do |user_hash|
+      "#{user_hash[:name]} - #{self.is_adult(user_hash[:age])}"
+    end
+  end
+
+  def is_adult(age)
+    age >= 18 ? 'adult' : 'child'
+  end
+end
+
+class ArticlePicker
+  attr_reader :test_articles
+  def initialize
+    @test_articles = [
+        {
+            title: "Awesome",
+            featured: true,
+            comments: [
+                { content: "This is awesome" },
+                { content: "Didn't know that!" }
+            ]
+        },
+        {
+            title: "Interesting article",
+            featured: false,
+            comments: [
+                { content: "Good one" }
+            ]
+        },
+        {
+            title: "Article about weather",
+            featured: true,
+            comments: []
+        }
+    ]
+  end
+  def pick(articles)
+    filtered_articles = articles.select do |article_item|
+      article_item[:featured] && article_item[:comments].size > 0
+    end.map do |filtered_article_item|
+      filtered_article_item[:title]
+    end
+  end
 end
